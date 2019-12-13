2ac2b2f36cf7af17fc6c7300f55348b652a6949f
Thomas Nguyen <TNguyen@spartaglobal.com>
2019-12-13 12:02:07 +0000

    Added more methods to set the values of passenger table and then included a while loop in the run file to allow the user to choose what they want the program to do - print all flights, search for a flight ID or adding a passenger.

--- db_flight_oop.py
+++ db_flight_oop.py
@@ -15,3 +15,32 @@ class AEFlights(MSDBConnection):
         query = f"SELECT * FROM Flight WHERE flightID = {id}"
         result = self._MSDBConnection__sql_query(query)
         return result
+
+    #Creating a passenger
+    def set_passenger_id(self):
+        passenger_id = int(input('Select ID for passenger'))
+        return passenger_id
+    #
+    def set_passenger_name(self):
+        passenger_name = input('Select name for passenger')
+        return passenger_name
+
+    def set_passport_num(self):
+        passport_num = input('Enter the passport number')
+        return passport_num
+
+    def add_passenger(self, passenger_id, passenger_name, passport_num):
+        # passenger_name = input('Select name for passenger')
+        # passport_num = input('Enter the passport number')
+        query = f"INSERT INTO Passenger VALUES({passenger_id}, '{passenger_name}', '{passport_num}')"
+        result = self._MSDBConnection__sql_query(query)
+        self.docker_AirportEx.commit()
+        return result
+
+# table_flights = AEFlights()
+#
+# passenger_id = table_flights.set_passenger_id()
+# passenger_name = table_flights.set_passenger_name()
+# passport_num = table_flights.set_passport_num()
+# passenger_1 = table_flights.add_passenger(passenger_id, passenger_name, passport_num)
+
--- run_file.py
+++ run_file.py
@@ -3,3 +3,37 @@ from flight_trip_class import *
 from db_flight_oop import *
 flight_table = AEFlights()
 
+
+while True:
+    print('Choose your option')
+    print('1 - Print all flights')
+    print('2 - Check for flight using a flight ID')
+    print('3 - Create a passenger (INSERT)')
+    print('4 - Add a passenger to a flight (ALTER / UPDATE)')
+    userInput = input('Enter your option number: ')
+    if userInput == '1':
+        # print('Create a passenger')
+        # passengerName = input('Enter the passengers name')
+        # passportNum = input('Enter the passport number')
+        # newPassenger = passengerName, passportNum
+        # add_passenger(newPassenger)
+        print_flight = flight_table.print_all()
+        print(print_flight.fetchall())
+    elif userInput == '2':
+        id = flight_table.set_id()
+        read_flight = flight_table.read_one(id)
+        print(read_flight.fetchone())
+    elif userInput == '3':
+        passenger_id = flight_table.set_passenger_id()
+        passenger_name = flight_table.set_passenger_name()
+        passport_num = flight_table.set_passport_num()
+        print('Passenger successfully added')
+        passenger_1 = flight_table.add_passenger(passenger_id, passenger_name, passport_num)
+        break
+    elif userInput == '4':
+        pass
+    elif 'bye' in userInput or 'exit' in userInput:
+        print('Goodbye! Thank you')
+        break
+    else:
+        print("I didn't quite catch that. Please choose a valid option")
